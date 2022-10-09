40abe11e63c7e37d70c0af78629a4349ac89678b
StefanPacola <stevo.pacola@gmail.com>
2022-10-09 19:28:19 +0200

    created fading in effect applied on sections

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("section").hide()
+    $(".bright-orange-section").fadeIn(2000)
+    $(".dark-cyan-section").fadeIn(2000)
+    $(".very-dark-cyan-section").fadeIn(2000)
+});
+      
+
+
+
+
+ 
